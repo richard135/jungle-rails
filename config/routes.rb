2057b02217b92320c6d23a65017116478c6ed0fd
richard135 <richardhsieh1@hotmail.com>
2017-03-16 02:43:24 +0000

    Added login forms, yet to be completed

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,16 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show]
   end
 
+  resources :users, only: [:create, :new]
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  # get '/signup' => 'users#new'
+  # post '/users' => 'users#create'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
